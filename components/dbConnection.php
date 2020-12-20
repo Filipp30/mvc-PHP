9898fc81d801eb52482d985447623fece22f116e
Filipp <filipp-tts@outlook.com>
2020-12-20 16:06:08 +0100

    ???

--- components/dbConnection.php
+++ components/dbConnection.php
@@ -4,9 +4,9 @@
     class dbConnection{
 
         public static function getDbConnection(){
-            $db_name="mvc";
-            $dsn = 'mysql:host=localhost:3308;dbname='.$db_name;
-            $pdo = new PDO($dsn,'root','root');
+            $db_name="us409094_mvc";
+            $dsn = 'mysql:host=us409094.mysql.tools;dbname='.$db_name;
+            $pdo = new PDO($dsn,'us409094_mvc','42(8s*eEiZ');
             return $pdo;
         }
     }
